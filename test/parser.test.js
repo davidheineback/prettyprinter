04d377538d48ca0547681e62939b09bab818121f
David Heinebäck <du222aa@student.lnu.se>
2021-10-04 15:38:56 +0200

    refactor: move todo to classes and and simple tests for sentences

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,28 +1,31 @@
 import Parser from "../src/parser/Parser.js"
 
-describe('Tests Parser', () => {
+describe('Tests Document class.', () => {
 
+  it('TC1 - running method getAllSentences should return an array with all sentences', () => {
+    const stringToTest = 'Hello World. Hello again! Hello?'
+    const document = new Parser('stringToTest')
+    expect(document.getAllSentences().length()).toEqual(3)
+  })
 
-  it('TC1 - First Parser test', () => {
+  it('TC2 - running method getAllRegularSentences should return an array with all regular sentences', () => {
     const document = new Parser()
-    const stringToTest = 'Hello World.'
-    expect(document.parse().getType()).toEqual('Regular')
-    expect(document.parse().getAll().length()).toEqual(1)
+    const stringToTest = 'Hello World. Hello again! Hello?'
+    expect(document.getType()).toEqual('REGULAR')
+    expect(document.getAllRegularSentences().length()).toEqual(1)
+  })
+
+  it('TC3 - running method getAllExclamationSentences should return an array with all exclamation sentences', () => {
+    const document = new Parser()
+    const stringToTest = 'Hello World. Hello again! Hello?'
+    expect(document.parse().getType()).toEqual('EXCLAMATION')
+    expect(document.parse().getAllExclamationSentences().length()).toEqual(1)
+  })
+
+  it('TC4 - running method getAllExclamationSentences should return an array with all exclamation sentences', () => {
+    const document = new Parser()
+    const stringToTest = 'Hello World. Hello again! Hello?'
+    expect(document.parse().getType()).toEqual('QUESTION')
+    expect(document.parse().getAllQuestionSentences().length()).toEqual(1)
   })
 })
-
-// Klassen Document skall kunna
-// Vi kan be om samtliga meningar oavsett typ. Vi får dem i samma följd som dokumentet 
-  //TODO: create getAll()
-// Vi kan be om alla vanliga meningar (som avslutas med punkt).
-  //TODO: create getAllRegularSentences()
-// Vi kan be om alla frågor (som avslutas med frågetecken).
-  //TODO: create getAllQuestionSentences()
-// Vi kan be om alla utrop (som avslutas med utropstecken).
-  //TODO: create getAllExclamationSentences()
-
-
-// Klasserna för “en vanlig mening”, uttrop och frågor skall alla ha funktionalitet för att:
-    // stega igenom WORD-objekt som meningen består av.
-    // Få hela meningen som en enda sträng med “rätt” avslutande tecken(.?!) 
-    // de enskilda orden separeras med ett enda mellanslag oavsett vilka whitespace som användes i orginaltexten.
