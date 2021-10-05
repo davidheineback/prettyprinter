d51778079e6135f3979f1c677cf1db7aed027e34
David Heinebäck <du222aa@student.lnu.se>
2021-10-05 21:46:59 +0200

    refactor: remove obsolete code

--- src/parser/Document.js
+++ src/parser/Document.js
@@ -26,24 +26,6 @@ export default class Document {
       if (token.tokenType !== 'WORD') {
         this.#sentences.add({'type': `${token.tokenType}`, 'sentence': this.#currentSentence})
       }
-      // switch(token.tokenType) {
-      //   case 'WORD':
-      //     break
-      //   case 'DOT':
-      //     this.#sentences.add({'type': 'REGULAR', 'sentence': this.#currentSentence})
-      //     this.#currentSentence = []
-      //     break
-      //   case 'EXCLAMATION':
-      //     this.#sentences.add({'type': 'EXCLAMATION', 'sentence': this.#currentSentence})
-      //     this.#currentSentence = []
-      //     break
-      //   case 'QUESTION':
-      //     this.#sentences.add({'type': 'QUESTION', 'sentence': this.#currentSentence})
-      //     this.#currentSentence = []
-      //     break
-      //   default:
-      //     break
-      // }
         this.#tokenContainer.setActiveTokenToNext()
     }
     return 
@@ -54,15 +36,7 @@ export default class Document {
   }
 
   getAllRegularSentences() {
-    const regularSentences = this.#sentences.getSentences().filter(sentence => sentence.type === 'DOT')
-    regularSentences.forEach(sentence => {
-      let fullSentence = ''
-      sentence.sentence.forEach(component => {
-        fullSentence += `${component} `
-      })
-      sentence.sentence = fullSentence.replace(/.{0,3}$/, '.')
-    })
-    return regularSentences
+    this.#sentences.getSentences().filter(sentence => sentence.type === 'DOT')
   }
 
   getAllExclamationSentences() {
@@ -72,10 +46,4 @@ export default class Document {
   getAllQuestionSentences() {
     console.log(this.#sentences.getSentences().filter(sentence => sentence.type === 'QUESTION'))
   }
-
-  // setNewStringToParse(stringToParse) {
-  //   this.#stringToParse = stringToParse
-  //   this.parse()
-  // }
-
 }
