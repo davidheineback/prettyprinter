a1caf54eb1c0a4964f3b890a6348226c79593190
David Heinebäck <du222aa@student.lnu.se>
2021-10-06 16:15:44 +0200

    refactor: add readline to index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,62 @@
 import Document from './parser/Document.js'
+import readline from 'readline'
 
-const document = new Document()
-document.parse('Hej på dig. Hallooo ! Hej       hej. Frågor på det?')
-let sentences = document.getAllRegularSentences()
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-sentences.forEach(sentence => {
-  console.log('\x1b[36m\x1b[1m%s\x1b[0m', sentence.getSenteceAsObject())
+rl.question("Write a sentence as a string ", function(string) {
+      const document = new Document()
+      document.parse(string)
+      let sentences = document.getAllRegularSentences()
+      
+      
+      sentences.forEach(sentence => {
+        console.log('\x1b[36m\x1b[1m%s\x1b[0m', sentence.getSentenceAsString())
+      })
+
+      sentences = document.getAllExclamationSentences()
+
+      sentences.forEach(sentence => {
+        console.log('\x1b[33m\x1b[1m\x1b[4m%s\x1b[0m', sentence.getSentenceAsString())
+      })
+
+      sentences = document.getAllQuestionSentences()
+
+      sentences.forEach(sentence => {
+        console.log('\x1b[35m\x1b[7m%s\x1b[0m', sentence.getSentenceAsString())
+      })
+
+      rl.close()
 })
 
-sentences.forEach(sentence => {
-  console.log('\x1b[36m\x1b[1m%s\x1b[0m', sentence.getSentenceAsString())
+rl.on("close", function() {
+    process.exit(0);
 })
 
-sentences = document.getAllExclamationSentences()
+// const document = new Document()
+// document.parse('Hej på dig. Hallooo ! Hej       hej. Frågor på det?')
+// let sentences = document.getAllRegularSentences()
 
-sentences.forEach(sentence => {
-  console.log('\x1b[33m\x1b[1m\x1b[4m%s\x1b[0m', sentence.getSentenceAsString())
-})
 
-sentences = document.getAllQuestionSentences()
+// sentences.forEach(sentence => {
+//   console.log('\x1b[36m\x1b[1m%s\x1b[0m', sentence.getSentenceAsString())
+// })
 
-sentences.forEach(sentence => {
-  console.log('\x1b[35m\x1b[7m%s\x1b[0m', sentence.getSentenceAsString())
-})
+
+
+// sentences = document.getAllExclamationSentences()
+
+// sentences.forEach(sentence => {
+//   console.log('\x1b[33m\x1b[1m\x1b[4m%s\x1b[0m', sentence.getSentenceAsString())
+// })
+
+// sentences = document.getAllQuestionSentences()
+
+// sentences.forEach(sentence => {
+//   console.log('\x1b[35m\x1b[7m%s\x1b[0m', sentence.getSentenceAsString())
+// })
 
 
 // Reverse = "\x1b[7m" switch background and foreground
