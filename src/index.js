cc8addf4466cacc6e98e4b643ef87987d159bd04
David Heinebäck <du222aa@student.lnu.se>
2021-10-06 15:20:44 +0200

    refactor index to debugg with console.logs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,34 @@ import Document from './parser/Document.js'
 
 const document = new Document()
 document.parse('Hej på dig. Hallooo ! Hej       hej. Frågor på det?')
-console.log(document.getAllRegularSentences())
-document.getAllExclamationSentences()
-document.getAllQuestionSentences()
+let sentences = document.getAllRegularSentences()
 
+sentences.forEach(sentence => {
+  console.log('\x1b[36m\x1b[1m%s\x1b[0m', sentence.getSenteceAsObject())
+})
+
+sentences.forEach(sentence => {
+  console.log('\x1b[36m\x1b[1m%s\x1b[0m', sentence.getSentenceAsString())
+})
+
+sentences = document.getAllExclamationSentences()
+
+sentences.forEach(sentence => {
+  console.log('\x1b[33m\x1b[1m\x1b[4m%s\x1b[0m', sentence.getSentenceAsString())
+})
+
+sentences = document.getAllQuestionSentences()
+
+sentences.forEach(sentence => {
+  console.log('\x1b[35m\x1b[7m%s\x1b[0m', sentence.getSentenceAsString())
+})
+
+
+// Reverse = "\x1b[7m" switch background and foreground
+// Bright = "\x1b[1m" sets bold and bright color
+// Underscore = "\x1b[4m"
+
+//Foreground colors:
+// FgYellow = "\x1b[33m"
+// FgMagenta = "\x1b[35m"
+// FgCyan = "\x1b[36m"
