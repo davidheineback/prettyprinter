92484895613d0a90151d2398a6131a4c5250ac1c
David Heinebäck <du222aa@student.lnu.se>
2021-10-07 14:50:48 +0200

    refactor: remove readline from PrettyPrinter and add to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import PrettyPrinter from './printer/PrettyPrinter.js'
+import readline from 'readline'
 
+const printer = new PrettyPrinter()
 
-  const printer = new PrettyPrinter()
+const readCommandLine = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-  printer.print()
+readCommandLine.question('Write a document as a string:', (document) => {
+  printer.print(document)
+  readCommandLine.close()
+})
+
+readCommandLine.on("close", () => process.exit(0))
