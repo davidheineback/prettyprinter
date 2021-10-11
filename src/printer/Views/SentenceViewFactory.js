e072387665c894e582741fc165ad2edfe09d105d
David Heinebäck <du222aa@student.lnu.se>
2021-10-11 17:39:05 +0200

    add: SentenceViewFactory

---
+++ src/printer/Views/SentenceViewFactory.js
@@ -0,0 +1,28 @@
+import RegularSentence from '../../parser/sentence/RegularSentence.js'
+import ExclamationSentence from '../../parser/sentence/ExclamationSentence.js'
+import QuestionSentence from '../../parser/sentence/QuestionSentence.js'
+import RegularSentenceView from './RegularSentenceView.js'
+import ExclamationSentenceView from './ExclamationSentenceView.js'
+import QuestionSentenceView from './QuestionSentenceView.js'
+
+export default class SentenceViewFactory {
+  
+  constructor () {
+  }
+
+  createSentenceView(sentence) {
+    switch(true) {
+      case sentence instanceof RegularSentence:
+        return new RegularSentenceView
+      case sentence instanceof ExclamationSentence:
+        return new ExclamationSentenceView
+      case sentence instanceof QuestionSentence:
+        return new QuestionSentenceView
+      default:
+        break
+    }
+  }
+
+
+
+}
