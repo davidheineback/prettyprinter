e520d4067109c62c6fea6268537e9303369c96a0
David Heinebäck <du222aa@student.lnu.se>
2021-10-12 08:10:28 +0200

    refactor: change code to implement sentenceviewfactory

--- src/printer/PrettyPrinter.js
+++ src/printer/PrettyPrinter.js
@@ -1,25 +1,25 @@
 import Document from '../parser/Document.js'
-import Styles from './styles/Styles.js'
 import SentenceViewFactory from './Views/SentenceViewFactory.js'
 
 
 export default class PrettyPrinter {
 
   print(string) {
-    Styles.printDescriptionOfStyles()
-    
       try {
         const document = new Document()
         document.parse(string)
+        const sentenceViewFactory = new SentenceViewFactory()
+        sentenceViewFactory.printDescriptionOfStyles()
         
         let sentences = document.getAllSentences()
         sentences.forEach((sentence, index) => {
-          const sentenceView = new SentenceViewFactory().createSentenceView(sentence)
+          const sentenceView = sentenceViewFactory.createSentenceView(sentence)
           sentenceView.print(sentence.getSentenceAsString(), index)
         })
       
       } catch (error) {
-        console.error(Styles.getStyleForTypeOf(error),error.message)
+        const sentenceViewFactory = new SentenceViewFactory()
+        sentenceViewFactory.createSentenceView(error).print(error.message)
       }
   }
 }
