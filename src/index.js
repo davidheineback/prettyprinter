24e1e21abd1f52e8083b8f80131630c39870c39b
David Heinebäck <du222aa@student.lnu.se>
2021-10-13 15:29:27 +0200

    refactor: move logic from index to parser as facade class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-import PrettyPrinter from './printer/PrettyPrinter.js'
-import readline from 'readline'
+import Parser from './parser/Parser.js'
 
-const printer = new PrettyPrinter()
-
-const readCommandLine = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-readCommandLine.question('Write a document as a string:', (document) => {
-  printer.print(document)
-  readCommandLine.close()
-})
-
-readCommandLine.on("close", () => process.exit(0))
+new Parser()
