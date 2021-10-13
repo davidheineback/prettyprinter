615f728b2a934db1904dd14053e0c7dbce24f329
David Heinebäck <du222aa@student.lnu.se>
2021-10-13 19:59:32 +0200

    refactor: rename methods in validator and implement length validation

--- src/parser/Validator.js
+++ src/parser/Validator.js
@@ -1,14 +1,18 @@
 export default class Validator {
+  #validTypes
+  #validLength
+
   constructor() {
-    this.validTypes = ['DOT', 'EXCLAMATION', 'QUESTION']
+    this.#validTypes = ['DOT', 'EXCLAMATION', 'QUESTION']
+    this.#validLength = 1
   }
 
-  sentenceContainsAtLeastOneWord() {
-    //check if string qualify as sentenece
+  isValidSentenceLength(length) {
+    return length > this.#validLength
   }
 
   isValidEndToken(type) {
-    return this.validTypes.includes(type)
+    return this.#validTypes.includes(type)
   }
 
   isInValidEndOfSentence(newTokenType, previousTokenType) {
