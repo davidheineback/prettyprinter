cf4506effe90a8f9839c0897654067f070b3fa9d
David Heinebäck <du222aa@student.lnu.se>
2021-10-14 10:01:31 +0200

    refactor: move try catch to Parser and add recursion of main method on error

--- src/parser/Parser.js
+++ src/parser/Parser.js
@@ -1,15 +1,22 @@
 import PrettyPrinter from '../printer/PrettyPrinter.js'
+import SentenceViewFactory from '../printer/views/SentenceViewFactory.js'
 import Readline from './ReadLine.js'
 
 export default class Parser {
   #printer = new PrettyPrinter()
-  #reader = new Readline()
 
   constructor() {
     this.#main()
   }
 
   async #main() {
-    this.#printer.print(await this.#reader.getDocument())
+    try {
+      const reader = new Readline()
+      this.#printer.print(await reader.getDocument())
+    } catch (error) {
+      const sentenceViewFactory = new SentenceViewFactory()
+      sentenceViewFactory.createSentenceView(error).print(error.message)
+      this.#main()
+    }
   }
 }
--- src/printer/PrettyPrinter.js
+++ src/printer/PrettyPrinter.js
@@ -5,7 +5,6 @@ import SentenceViewFactory from './views/SentenceViewFactory.js'
 export default class PrettyPrinter {
 
   print(string) {
-      try {
         const document = new Document()
         document.parse(string)
         const sentenceViewFactory = new SentenceViewFactory()
@@ -16,10 +15,5 @@ export default class PrettyPrinter {
           const sentenceView = sentenceViewFactory.createSentenceView(sentence)
           sentenceView.print(sentence.getSentenceAsString(), index)
         })
-      
-      } catch (error) {
-        const sentenceViewFactory = new SentenceViewFactory()
-        sentenceViewFactory.createSentenceView(error).print(error.message)
-      }
   }
 }
