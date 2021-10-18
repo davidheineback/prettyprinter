297904ce5bafce52d5c2c9a289dbebd6b7d03bce
David Heinebäck <du222aa@student.lnu.se>
2021-10-18 09:02:39 +0200

    refactor: change test after implementation of 'law of demeter'

--- test/parser.test.js
+++ test/parser.test.js
@@ -15,51 +15,51 @@ describe('TC1 Tests Document class with string "Hello World. Hello again! Hello!
   })
 
   it('TC1.2 - running method getAllRegularSentences should return an array only containing sentences of type RegularSentence', () => {
-    document.getAllSentences().getAllRegularSentences().forEach(sentence => {
+    document.getAllRegularSentences().forEach(sentence => {
       expect(sentence instanceof RegularSentence).toEqual(true)
     })
   })
 
   it('TC1.3 - running method getSentenceAsObject on a RegularSentence should return type "REGULAR" ', () => {
-    document.getAllSentences().getAllRegularSentences().forEach(sentence => {
+    document.getAllRegularSentences().forEach(sentence => {
       expect(sentence.getSentenceAsObject().type).toEqual('REGULAR')
     })
   })
 
   it('TC1.4 - running method getSentenceAsString on the first RegularSentence should return string "Hello World." ', () => {
-    expect(document.getAllSentences().getAllRegularSentences()[0].getSentenceAsString()).toEqual('Hello World.')
+    expect(document.getAllRegularSentences()[0].getSentenceAsString()).toEqual('Hello World.')
   })
 
   it('TC1.5 - running method getAllExclamationSentences should return an array only containing sentences of type ExclamationSentence', () => {
-    document.getAllSentences().getAllExclamationSentences().forEach(sentence => {
+    document.getAllExclamationSentences().forEach(sentence => {
       expect(sentence instanceof ExclamationSentence).toEqual(true)
     })
   })
 
   it('TC1.6 - running method getSentenceAsObject on a ExclamationSentence should return type "EXCLAMATION" ', () => {
-    document.getAllSentences().getAllExclamationSentences().forEach(sentence => {
+    document.getAllExclamationSentences().forEach(sentence => {
       expect(sentence.getSentenceAsObject().type).toEqual('EXCLAMATION')
     })
   })
 
   it('TC1.7 - running method getSentenceAsString on the second ExclamationSentence should return string "Hello!" ', () => {
-    expect(document.getAllSentences().getAllExclamationSentences()[1].getSentenceAsString()).toEqual('Hello!')
+    expect(document.getAllExclamationSentences()[1].getSentenceAsString()).toEqual('Hello!')
   })
 
   it('TC1.8 - running method getAllQuestionSentences should return an array only containing sentences of type QuestionSentence', () => {
-    document.getAllSentences().getAllQuestionSentences().forEach(sentence => {
+    document.getAllQuestionSentences().forEach(sentence => {
       expect(sentence instanceof QuestionSentence).toEqual(true)
     })
   })
 
   it('TC1.9 - running method getSentenceAsObject on a QuestionSentence should return type "QUESTION" ', () => {
-    document.getAllSentences().getAllQuestionSentences().forEach(sentence => {
+    document.getAllQuestionSentences().forEach(sentence => {
       expect(sentence.getSentenceAsObject().type).toEqual('QUESTION')
     })
   })
 
   it('TC1.10 - running method getSentenceAsString on the second QuestionSentence should return string "Anyone?" ', () => {
-    expect(document.getAllSentences().getAllQuestionSentences()[1].getSentenceAsString()).toEqual('Anyone?')
+    expect(document.getAllQuestionSentences()[1].getSentenceAsString()).toEqual('Anyone?')
   })
 })
 
@@ -72,15 +72,15 @@ describe('TC2 Test that length is as expected with string "Hello World. Hello ag
   })
 
   it('TC2.2 - running method getAllRegularSentences should have length 1', () => {
-    expect(document.getAllSentences().getAllRegularSentences().length).toEqual(1)
+    expect(document.getAllRegularSentences().length).toEqual(1)
   })
 
   it('TC2.3 - running method getAllExclamationSentences should have length 2', () => {
-    expect(document.getAllSentences().getAllExclamationSentences().length).toEqual(2)
+    expect(document.getAllExclamationSentences().length).toEqual(2)
   })
 
   it('TC2.4 - running method getAllQuestionSentences should have length 2', () => {
-    expect(document.getAllSentences().getAllQuestionSentences().length).toEqual(2)
+    expect(document.getAllQuestionSentences().length).toEqual(2)
   })
 })
 
@@ -90,12 +90,12 @@ describe('TC3 Test Error handling', () => {
   it('TC3.1 - Parsing a sentence without correct ending throws error', () => {
     expect(() => {
       document.parse('Hello World')
-    }).toThrow()
+    }).toThrow('Syntax error: \"Hello World\" doesn\'t have a valid end character')
   })
 
   it('TC3.2 - Parsing a sentence without containing incorrect sentence build throws error', () => {
     expect(() => {
       document.parse('Hello World..')
-    }).toThrow()
+    }).toThrow('Syntax error: \".\" is not a valid syntax for a sentence')
   })
 })
